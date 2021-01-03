3c3bc7d82529bd7383e70678d4d2cac443d9b29f
paulhorowitz <paulbhorowitz@gmail.com>
2021-01-03 17:21:06 +1030

    Change table format to card format

--- index.js
+++ index.js
@@ -15,63 +15,39 @@ function addBookToLibrary(title, author, noOfPages, completed) {
 
 function renderPage() {
     //remove current render
-    while (cardEl.firstChild) {
-        cardEl.removeChild(cardEl.firstChild)
+    while (containerEl.firstChild) {
+        containerEl.removeChild(containerEl.firstChild)
     }
-    
-    const titleHead = document.createElement("th");
-    const authorHead = document.createElement("th");
-    const pagesHead = document.createElement("th");
-    const completedHead = document.createElement("th");
-    const removeHead = document.createElement("th");
-    const rowEl2 = document.createElement("tr")
 
     //create header row for table
-    cardEl.appendChild(rowEl2)
-    rowEl2.appendChild(titleHead);
-    rowEl2.appendChild(authorHead)
-    rowEl2.appendChild(pagesHead);
-    rowEl2.appendChild(completedHead);
-    rowEl2.appendChild(removeHead);
 
     myLibrary.forEach(book => {
+        const containerEl = document.getElementById("container")
+        const cardEl = document.createElement("div");
 
-        const rowEl = document.createElement("tr");
-
-        
-        const title = document.createElement("td");
-        const author = document.createElement("td");
-        const noOfPagesEl = document.createElement("td");
-        const completedRowEl = document.createElement("td")
+        const title = document.createElement("h4");
+        const author = document.createElement("h6");
+        const noOfPagesEl = document.createElement("h7");
+        const completedRowEl = document.createElement("h7")
         const completedEl = document.createElement("button");
-        const buttonRowEl = document.createElement("td");
-        const buttonEl = document.createElement("button");
-        
+        const buttonEl = document.createElement("i");
 
-
-        titleHead.innerHTML = 'Title';
-        authorHead.innerHTML = 'Author';
-        pagesHead.innerHTML = 'Pages';
-        completedHead.innerHTML = 'Completed?';
-
-
-        console.log(book)
-        cardEl.appendChild(rowEl);
-        rowEl.appendChild(title);
+        containerEl.classList.add("container")
+        cardEl.classList.add("card")
+        containerEl.appendChild(cardEl);
+        cardEl.appendChild(title);
         title.innerHTML = book.title;
-        rowEl.appendChild(author);
-        author.innerHTML = book.author;
-        rowEl.appendChild(noOfPagesEl);
-        noOfPagesEl.innerHTML = book.noOfPages;
-        rowEl.appendChild(completedRowEl);
+        cardEl.appendChild(author);
+        author.innerHTML = 'by ' + book.author;
+        cardEl.appendChild(noOfPagesEl);
+        noOfPagesEl.innerHTML = book.noOfPages + " pages";
+        cardEl.appendChild(completedRowEl);
         completedRowEl.appendChild(completedEl);
-        completedEl.innerHTML = book.completed
-        completedEl.setAttribute("class", "comp")
-        rowEl.appendChild(buttonRowEl)
-        buttonRowEl.appendChild(buttonEl)
-        buttonEl.innerHTML = 'remove';
+        completedEl.innerHTML = book.completed;
+        completedEl.setAttribute("class", "comp");
+        cardEl.appendChild(buttonEl);
         buttonEl.setAttribute("id", book.id);
-        buttonEl.setAttribute("class", "rmv")
+        buttonEl.setAttribute("class", "far fa-trash-alt rmv")
     })
 
     completedButton();
@@ -80,7 +56,7 @@ function renderPage() {
 }
 
 function removeButton() {
-    const removeButton = document.querySelectorAll("button.rmv");
+    const removeButton = document.querySelectorAll("i.rmv");
     var removeButtonArray = Array.from(removeButton)
     removeButtonArray.forEach(element => {
         element.addEventListener("click", function(e) {
@@ -95,11 +71,11 @@ function completedButton() {
     var changeCompletedBut = Array.from(completedBut)
     changeCompletedBut.forEach(element => {
         element.addEventListener("click", function(e) {
-            if (element.innerHTML == 'yes') {
-                element.innerHTML = 'no ';
+            if (element.innerHTML == 'completed') {
+                element.innerHTML = 'incomplete ';
             } 
             else {
-                element.innerHTML = 'yes';
+                element.innerHTML = 'completed';
             }
         }
         )
@@ -128,10 +104,10 @@ function removeRecord(id) {
 let myLibrary = [];
 
 //sample book
-addBookToLibrary('Harry Potter', 'J.K Rowling', 200, 'yes');
-addBookToLibrary('1984', 'George Orwell', 333, 'no');
+addBookToLibrary('Harry Potter', 'J.K Rowling', 200, 'completed');
+addBookToLibrary('1984', 'George Orwell', 333, 'incomplete');
 
-const cardEl = document.getElementById('card');
+const containerEl = document.getElementById('container');
 const formBtnEl = document.getElementById("form-button");
 var titleEl = document.getElementById("add-title");
 var authorEl = document.getElementById("add-author");
@@ -156,10 +132,10 @@ formBtnEl.addEventListener("click", function(e) {
 
 checkbox.addEventListener('change', function(e) {
     if (checkbox.checked) {
-        this.value = 'yes';
+        this.value = 'completed';
     }
     else {
-        this.value = 'no';
+        this.value = 'incomplete';
     }
     
 })
